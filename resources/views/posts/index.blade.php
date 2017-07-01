8ffbd49a7df7c0a5bce671c4c6762faefeba4c0d
Ricardo Caro <ricardo.caro9@hotmail.com>
2017-06-30 22:03:25 -0500

    comit de inicio

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,34 @@
+
+
+@extends('leyouts.app')
+
+@section('content')
+
+     <div class="row">
+            <div class="col-md-12">
+                <h2>Title</h2>
+                <p>Este es un ejemplo</P>  
+            </div>
+        </div>
+
+        <hr>
+        
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Title</h2>
+                <p>Este es un ejemplo</P>  
+            </div>
+        </div>
+
+        <hr>
+        
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Title</h2>
+                <p>Este es un ejemplo</P>  
+            </div>
+        </div>
+
+@endsection
+
+
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,16 @@
+
+
+@extends('leyouts.app')
+
+@section('content')
+
+     <div class="row">
+            <div class="col-md-12">
+                <h2>Title {{ $id }}</h2>
+                <p>Descripcion</P>
+                <p>Posted 5 horas</p>  
+            </div>
+        </div>
+@endsection
+
+
